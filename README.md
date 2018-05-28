8f97109d5d6e30179c362e2e6194046fc42d53c2
nazarchykur <ChNDXXI@gmail.com>
2018-05-28 19:03:11 +0200

    add to github this StartTemplateGulp

---
+++ README.md
@@ -0,0 +1,10 @@
+OptimizedHTML is all-inclusive start HTML5 
+template  Gulp, Sass, Browsersync, 
+Autoprefixer, Clean-CSS, Uglify, 
+Rsync and Bower (libs path) support. 
+The template contains a .htaccess 
+file with caching rules for web server.
+
+Cross-browser compatibility: IE9+.
+
+The template uses a Sass with Sass or Scss syntax (optional).
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp           = require('gulp'),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify"),
+		rsync          = require('gulp-rsync'),
+		sourcemaps     = require('gulp-sourcemaps');
+
+gulp.task('common-js', function() {
+	return gulp.src([
+		'app/js/common.js',
+		])
+	//.pipe(concat('common.min.js'))
+	//.pipe(uglify()) // minify (optional)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'./app/libs/jquery/jquery-3.2.1.min.js',
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(sourcemaps.init())
+	.pipe(uglify()) // minify (optional)
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('app/js'))
+});
+
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass().on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	//.pipe(cleanCSS()) // (optional)
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['sass', 'js', 'common-js', 'browser-sync'], function() {
+	gulp.watch('app/sass/**/*.scss', ['sass']);
+	gulp.watch('libs/**/*.js', ['js-watch']);
+	gulp.watch('app/js/common.js', ['common-js']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+// (optional)
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin()))
+	.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('build', ['removedist', 'sass', 'js'], function() {
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		'app/css/main.min.css.map',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/scripts.min.js',
+		'app/js/scripts.min.js.map',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildCommonJs = gulp.src([
+		'app/js/common.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
